622f1eb84c1fcfbe1e19d0c3e3df6746f21a0eec
Administrador <natimartalvarez@gmail.com>
2024-06-27 14:08:05 -0300

    mi primer commit

---
+++ index.ts
@@ -0,0 +1,27 @@
+class Album {
+  title: string;
+  songs: string[];
+  constructor(title, songs: string[]) {
+    this.title = title;
+    this.songs = songs;
+  }
+}
+class Banda {
+  members: string[];
+  albums: Album[];
+  constructor(members: string[], albums: Album[]) {
+    this.members = members;
+    this.albums = albums;
+  }
+}
+
+function main() {
+  const unAlbum = new Album("album de nati", []);
+
+  const unaBanda = new Banda(["nati"], [unAlbum, unAlbum, unAlbum]);
+
+  //   console.log(unaBanda.members);
+  console.log(unaBanda.albums);
+}
+
+main();
